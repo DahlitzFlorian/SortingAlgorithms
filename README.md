13b4e60949ff0fc8aec5c614602242b4c28b73c8
florian dahlitz <f2dahlitz@freenet.de>
2017-04-12 11:29:06 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Sorting Algorithms #
+## Description ##
+This project includes a various number or sorting algorithms
+written in Python.
+
+version 0.1
+
+## Algorithms ## 
+Right now no algorithms included.
---
+++ SortingAlgorithms.py
@@ -0,0 +1,10 @@
+"""Sorting Algorithms
+
+    This file includes all sorting algorithms existing in
+    this project. It provides an interface to use them
+    easily to sort lists and dictionaries.
+
+    @author Florian Dahlitz
+"""
+class SortingAlgorithms:
+    pass
