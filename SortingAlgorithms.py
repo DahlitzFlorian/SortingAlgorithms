80dbf88f6225a6e2e0ae5b969aa1de060e5ed513
florian dahlitz <f2dahlitz@freenet.de>
2017-04-12 13:29:11 +0200

    Added Bubble-Sort & changed some identifier

--- SortingAlgorithms.py
+++ SortingAlgorithms.py
@@ -2,21 +2,35 @@
 
     This file includes all sorting algorithms existing in
     this project. It provides an interface to use them
-    easily to sort lists and dictionaries.
+    easily to sort lists.
 
     @author Florian Dahlitz
 """
-def swap(collection, a, b):
+def swap(list, a, b):
     """swap
 
-        Swaps two values of a given collection. Needs a pointer
-        to the collection and the two positions of the
+        Swaps two values of a given lists. Needs a pointer
+        to the lists and the two positions of the
         values to swap.
     """
-    tmp = collection[a]
-    collection[a] = collection[b]
-    collection[b] = tmp
+    tmp = list[a]
+    list[a] = list[b]
+    list[b] = tmp
+
+
+def bubble_sort(data):
+    """Bubble Sort
+
+        This algorithm sorts by comparing neighboring values.
+        Average: O(n^2)
+    """
+    for j in range(0, len(data)):
+        for i in range(0, len(data) - 1):
+            if data[i] > data[i+1]:
+                swap(data, i, i+1)
+    
+    return data
 
 
 class SortingAlgorithms:
-    pass
+    bubble_sort = staticmethod(bubble_sort)
