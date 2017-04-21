619576d7844e9559bf8eda88d12210bfc3238948
florian dahlitz <f2dahlitz@freenet.de>
2017-04-21 20:45:06 +0200

    modified setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='sort-algorithms',
-    version='1.0.1',
+    version='1.0.2',
     packages=['sort',],
     license='MIT',
     description='Sorting algorithms written in and made for Python lists and tuples',
@@ -11,10 +11,12 @@ setup(
     author_email='f2dahlitz@freenet.de',
     url='https://github.com/DahlitzFlorian/SortingAlgorithms',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
     keywords='sort algorithms sorting lists tuples',
 )
